3bff516e76b75f3e1083587348c779818feb2837
Fitz Bushnell <bushnelf@gmail.com>
2021-09-17 11:59:00 -0600

    initial commit

---
+++ LoadGameResults.py
@@ -0,0 +1,71 @@
+from neo4j import GraphDatabase as gd
+import dbcfg
+import sys
+
+class LoadGameResults():
+    def __init__(self, flnm):
+        self._flnm = flnm
+        self._drv = None
+        self._sess = None
+
+    def connect(self):
+        self._drv = gd.driver(dbcfg.uri, auth=(dbcfg.usr, dbcfg.pw))
+        self._sess = self._drv.session(database=dbcfg.db)
+
+    def disconnect(self):
+        self._drv.close()
+
+    def load2neo(self):
+        # CREATE CONSTRAINT uniq_game_id ON (g:Game) ASSERT g.gameid IS UNIQUE
+        # connect to Neo4j
+        # get data from file
+        # load data to Neo
+        # disconnect
+        self.connect()
+        savflnm = self._flnm.split("\\")[-1]
+        fl = open(self._flnm)
+        prevgame = ""
+        playpos = -1
+        for ln in fl:
+            flds = ln.strip().split()
+            gameid = flds[0]
+            if gameid != prevgame:
+                playpos = 1
+                prevgame = gameid
+            else:
+                playpos += 1
+            winloss = flds[1][0]
+            strats = "+".join(flds[3:-4])
+            scor = flds[-1]
+            # currently matching only on strategy set, not including weight
+            # cr = """
+            # MATCH (ss:StrategySet {{strategies: '{0}'}})
+            # MERGE (g:Game {{gameid: '{1}'}}) <-[:PLAYS_IN]-
+            #     (gp:GamePlayer {{winlossflag: '{2}', score: {3}}}) -[:USES]->
+            #     (ss)
+            # """.format(strats, gameid, winloss, scor)
+            # cr = """
+            # MATCH (ss:StrategySet {{strategies: '{0}'}})
+            # MERGE (g:Game {{gameid: '{1}'}}) <-[:PLAYS_IN]-
+            #     (gp:GamePlayer {{winlossflag: '{2}', score: {3}}})
+            # MERGE (gp)-[:USES]->(ss)
+            # """.format(strats, gameid, winloss, scor)
+            cr = """
+            MERGE (ss:StrategySet {{strategies: '{0}'}})
+            MERGE (g:Game {{gameid: '{1}', filename: '{4}'}})
+            CREATE (gp:GamePlayer {{position: {5}, winlossflag: '{2}', score: {3}}}) 
+            CREATE (g)<-[:PLAYS_IN]-(gp)
+            CREATE (gp)-[:USES]->(ss)
+            """.format(strats, gameid, winloss, scor, savflnm, playpos)
+            print(cr)
+            self._sess.run(cr)
+        fl.close()
+        self.disconnect()
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("usage: {0} gameResultsFile".format(sys.argv[0]))
+        sys.exit(-1)
+
+    lss = LoadGameResults(sys.argv[1])
+    lss.load2neo()
---
+++ LoadStratSet.py
@@ -0,0 +1,64 @@
+from neo4j import GraphDatabase as gd
+import dbcfg
+import sys
+
+class LoadStratSet():
+    def __init__(self, flnm):
+        self._flnm = flnm
+        self._drv = None
+        self._sess = None
+
+    def connect(self):
+        self._drv = gd.driver(dbcfg.uri, auth=(dbcfg.usr, dbcfg.pw))
+        self._sess = self._drv.session(database=dbcfg.db)
+
+    def disconnect(self):
+        self._drv.close()
+
+    def stdweight(self, strats):
+        # We only want a max of 6 "heavy" weights, with the rest being 1.
+        # So 9 would be 6 5 4 3 2 1 1 1 1 and
+        #    7 would be 6 5 4 3 2 1 1 and
+        #    4 would be 4 3 2 1.
+        stratcnt = len(strats.split("+"))
+        maxwgt = min(5, stratcnt)
+        weights = [max(r, 1) for r in range(maxwgt, maxwgt - stratcnt, -1)]
+        return ("".join([str(w) for w in weights]))
+
+    def load2neo(self):
+        # connect to Neo4j
+        # get data from file
+        # load data to Neo
+        # disconnect
+        self.connect()
+        savflnm = self._flnm.split("\\")[-1]
+        # create the source file name
+        cr1 = """
+        CREATE (:SourceFile {{filename: '{0}'}})
+        """.format(savflnm)
+        print(cr1)
+        self._sess.run(cr1)
+        fl = open(self._flnm)
+        for ln in fl:
+            flds = ln.strip().split(":")
+            if len(flds) >= 7:
+                (strats, winrt, cnt, z1, z1, z3, wgt) = flds
+            else:
+                strats = flds[0]
+                wgt = self.stdweight(strats)
+            cr2 = """
+            MATCH (sf:SourceFile {{filename: '{0}'}})
+            CREATE (ss:StrategySet {{strategies: '{1}', weight: {2}}})-[:FROM]->(sf)
+            """.format(savflnm, strats, wgt)
+            print(cr2)
+            self._sess.run(cr2)
+        fl.close()
+        self.disconnect()
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("usage: {0} strategySetFile".format(sys.argv[0]))
+        sys.exit(-1)
+
+    lss = LoadStratSet(sys.argv[1])
+    lss.load2neo()
